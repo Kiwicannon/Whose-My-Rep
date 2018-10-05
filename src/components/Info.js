e513bb0294a5352f7833ba164a070265beb08076
Steven Cannon <stevencannon01@gmail.com>
2018-10-04 20:33:01 -0600

    last css touches

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Rep = (props) => {
     return (
-        <div>
-            <h1>HERE</h1>
+        <div className='rep-info'>
+            <h1>Info</h1>
+            <span>{props.rep.first_name ? props.rep.first_name : 'First Name'}</span>
+            <span>{props.rep.last_name ? props.rep.last_name : 'Last Name'}</span>
+            <span>{props.rep.district ? props.rep.district : 'District'}</span>
+            <span>{props.rep.phone ? props.rep.phone : 'Phone'}</span>
+            <span>{props.rep.office ? props.rep.office : 'Office'}</span>
         </div>
     )
 }
--- src/components/Representatives.js
+++ src/components/Representatives.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Representatives.css'
 import Info from './Info';
-
 import stateList from './stateList.json';
-import Axios from 'axios';
 
 
 
@@ -11,72 +10,82 @@ export default class Representatives extends Component {
     constructor() {
         super();
         this.state = {
-            Representatives: [],
-            State: '',
-            Type: ''
+            representatives: [],
+            selectedRep: {
+                name: ''
+            },
+            state: '',
+            type: ''
         }
         this.handleClickSubmit = this.handleClickSubmit.bind(this)
+        this.setSelectedRep = this.setSelectedRep.bind(this)
     }
-    
+    setSelectedRep(e) {
+        e.first_name = e.name.split(' ')[0]
+        e.last_name = e.name.split(' ')[1]
+        this.setState({
+            selectedRep: e
+        })
+    }
+
     handleClickSubmit() {
-        console.log(this.state, `http://localhost:3000/${this.state.Type}/${this.state.State}`)
-        if (this.state.Type && this.state.State) {
-            if (this.state.Type === 'Representatives') {
-                Axios.get(`http://localhost:3000/representatives/${this.state.State}`)
+            this.setState({
+                selectedRep: {}
+            })
+            if (this.state.type === 'Representatives') {
+                axios.get(`http://localhost:3000/representatives/${this.state.state}`)
                 .then(( res ) => {
-                    console.log('res', res)
                     this.setState({
-                        Representatives: res.data.results
+                        representatives: res.data.results
                     })
                 })
                 
             }
-            Axios.get(`http://localhost:3000/senators/${this.state.State}`)
+            axios.get(`http://localhost:3000/senators/${this.state.state}`)
             .then(( res ) => {
                 this.setState({
-                    Representatives: res.data.results
+                    representatives: res.data.results
                 })
             })
-        }
     }
 
     render() {
-        const { State, Type, Representatives } = this.state;
-        const isEnabled =
-         State.length > 0 && 
-            Type.length > 0;
-        const Rep = Representatives.map(( e, i) => {
+        const { state, type, representatives, selectedRep } = this.state;
+        const Rep = representatives.map(( e, i) => {
             return (
-            <div className='rep-list' key={i}>
-                <h3>{ e.name }</h3>
-                <h3>{ e.party }</h3>
-            </div>
+            <span onClick={ () => this.setSelectedRep(e) } className='rep-list' key={i}>
+                <h4>{ e.name }</h4>
+                <h4>{ e.party }</h4>
+            </span>
             )
         })
-        const state = stateList.map(( e, i ) => {
+        const states = stateList.map(( e, i ) => {
             return <option key={i} value={e.abbreviation}>{ e.name }</option>
         })
         return (
             <div>
-                <select onChange={ (e) => this.setState({ Type: e.target.value }) }>
+                <select onChange={ (e) => this.setState({ type: e.target.value }) }>
                     <option value=''>Select Type</option>
                     <option value='Representatives'>Representative</option>
                     <option value='Senators'>Senator</option>
                 </select>
-                <select onChange={ (e) => this.setState({ State: e.target.value }) }>
+                <select onChange={ (e) => this.setState({ state: e.target.value }) }>
                     <option value=''>Select State</option>
-                    { state }
+                    { states }
                 </select>
-                <button disabled={!isEnabled} onClick={ this.handleClickSubmit }>Submit</button>
+                <button disabled={state && type ? false : true } onClick={ this.handleClickSubmit }>Submit</button>
 
-
-                <h2>List / <span className='list-header'>{ this.state.Type }</span> </h2> 
-                <div className='rep-header'>
-                    <h3>Name</h3>
-                    <h3>Party</h3>
+                <div className='rep-list-and-data'>
+                    <div className='reps'>
+                        <h2>List / <span className='list-header'>{ type }</span> </h2> 
+                            <span className='rep-header'>
+                                <h3>Name</h3>
+                                <h3>Party</h3>
+                            </span>
+                            { Rep }
+                    </div>
+                    < Info rep={ selectedRep }/>
                 </div>
-                { Rep }
-                { Info }
             </div>
         )
     }
